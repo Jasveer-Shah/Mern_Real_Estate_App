a7f4df4f465ed11c2bb9c762fd9f7bf649439e6a
Jasveer-Shah <talkjasveer@gmail.com>
2024-02-09 13:31:23 -0500

    added delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,9 @@
 import  express  from "express";
-import { createlisting } from "../controllers/listing.controller.js";
+import { createlisting, deleteListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createlisting);
+router.delete("/delete/:id", verifyToken, deleteListing)
 export default router;
