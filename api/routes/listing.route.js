1a0ab5cfb8b07508aca9b5d2b30f7d0deebb4291
Jasveer-Shah <talkjasveer@gmail.com>
2024-02-10 12:20:11 -0500

    search api route and header UI search form functionality completed

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,7 @@
 import  express  from "express";
-import { createlisting, deleteListing, updateListing, getListing } from "../controllers/listing.controller.js";
+import { createlisting, deleteListing, updateListing, getListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.post('/create', verifyToken, createlisting);
 router.delete("/delete/:id", verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get("/get", getListings)       // not protected route/ Public route / everyone can see listings
 
 export default router;
